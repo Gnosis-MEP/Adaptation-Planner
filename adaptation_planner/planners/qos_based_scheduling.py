f702133aab3f78b2f46eff2980be1924c71c40b4
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2021-03-10 12:14:44 +0000

    improving on the single best query-aware and congestion aware scheduling plan

--- adaptation_planner/planners/qos_based_scheduling.py
+++ adaptation_planner/planners/qos_based_scheduling.py
@@ -1,11 +1,30 @@
 import functools
 import itertools
+import math
+
 from ..conf import QUERY_SERVICE_CHAIN_FIELD
 from .scheduling import BaseSchedulerPlanner
 
 
 class BaseQoSSchedulerPlanner(BaseSchedulerPlanner):
     """Base class for scheduler planning"""
+
+    def workers_key_sorted_by_qos(self, worker_pool, qos_policy_name, qos_policy_value):
+        reverse = True if qos_policy_value == 'max' else False
+
+        metric_name = qos_policy_name
+        # latency actually will check for the oposity of throughput:
+        # min latency == max worker throughput
+        if qos_policy_name == 'latency':
+            metric_name = 'throughput'
+            reverse = False if reverse else True
+
+        sorted_workers = sorted(
+            worker_pool.keys(), key=lambda w_key: float(worker_pool[w_key]['monitoring'][metric_name]),
+            reverse=reverse
+        )
+
+        return sorted_workers
 
     def prepare_local_queries_entities(self, knowledge_queries):
         # get the query about the subscriber_query
@@ -54,23 +73,184 @@ class SingleBestForQoSSinglePolicySchedulerPlanner(BaseQoSSchedulerPlanner):
             ce_endpoint_stream_key
         )
         self.strategy_name = 'single_best'
+        self.adaptation_delta = 10
+        self.events_capacity_key = 'events_capacity'
 
-    def workers_key_sorted_by_qos(self, worker_pool, qos_policy_name, qos_policy_value):
-        reverse = True if qos_policy_value == 'max' else False
+    def get_bufferstream_planned_event_count(self, buffer_stream_entity):
+        fps = float(buffer_stream_entity['fps'])
+        num_events = self.adaptation_delta * fps
+        return math.ceil(num_events)
 
-        metric_name = qos_policy_name
-        # latency actually will check for the oposity of throughput:
-        # min latency == max worker throughput
-        if qos_policy_name == 'latency':
-            metric_name = 'throughput'
-            reverse = False if reverse else True
+    def calculate_worker_queue_space_percentage(self, worker):
+        return worker['resources']['planned']['queue_space'] / worker['monitoring']['queue_limit']
 
-        sorted_workers = sorted(
-            worker_pool.keys(), key=lambda w_key: float(worker_pool[w_key]['monitoring'][metric_name]),
-            reverse=reverse
+    def initialize_planned_worker_event_capacity(self, worker):
+        if self.events_capacity_key not in worker['resources']['planned']:
+            queue_size = int(worker['monitoring']['queue_size'])
+            throughput = float(worker['monitoring']['throughput'])
+            max_events_capacity = self.adaptation_delta * throughput
+            events_capacity = max_events_capacity - queue_size
+            worker['resources']['planned'][self.events_capacity_key] = events_capacity
+
+    def initialize_service_workers_planned_capacity(self, worker_pool):
+        for worker_key, worker in worker_pool.items():
+            self.initialize_planned_worker_event_capacity(worker)
+        return worker_pool
+
+    def is_worker_overloaded(self, worker):
+        return worker['resources']['planned'].get(self.events_capacity_key, 0) <= 0
+
+    def filter_overloaded_service_worker_pool_or_all_if_empty(self, worker_pool):
+        selected_worker_pool = dict(filter(
+            lambda kv: not self.is_worker_overloaded(kv[1]), worker_pool.items()
+        ))
+        if len(selected_worker_pool) == 0:
+            selected_worker_pool = worker_pool
+        return selected_worker_pool
+
+    # def get_dataflow_bottleneck(self, required_services, workers_keys, required_events):
+    #     prev_min = None
+    #     for dataflow_index, service in enumerate(required_services):
+    #         worker_key = workers_keys[dataflow_index]
+    #         worker = self.all_services_worker_pool[service][worker_key]
+    #         prev_min = worker
+
+    def update_workers_planned_resources(self, required_services, buffer_stream_plan, required_events):
+        for dataflow_index, service in enumerate(required_services):
+            worker_key = buffer_stream_plan[dataflow_index][0]
+            worker = self.all_services_worker_pool[service][worker_key]
+            events_capacity = worker['resources']['planned'].get(self.events_capacity_key, 0)
+            updated_events_capacity = events_capacity - required_events
+            worker['resources']['planned'][self.events_capacity_key] = updated_events_capacity
+
+    def create_buffer_stream_plan(self, buffer_stream_entity):
+        required_services = self.get_buffer_stream_required_services(buffer_stream_entity)
+        first_query = list(buffer_stream_entity['queries'].values())[0]
+        qos_policy_name, qos_policy_value = list(first_query['qos_policies'].items())[0]
+        required_events = self.get_bufferstream_planned_event_count(buffer_stream_entity)
+
+        buffer_stream_plan = []
+        for service in required_services:
+            worker_pool = self.all_services_worker_pool[service]
+            worker_pool = self.initialize_service_workers_planned_capacity(worker_pool)
+            selected_worker_pool = self.filter_overloaded_service_worker_pool_or_all_if_empty(worker_pool)
+
+            qos_sorted_workers_keys = self.workers_key_sorted_by_qos(
+                selected_worker_pool, qos_policy_name, qos_policy_value)
+
+            best_worker_key = qos_sorted_workers_keys[0]
+            buffer_stream_plan.append([best_worker_key])
+        self.update_workers_planned_resources(required_services, buffer_stream_plan, required_events)
+        buffer_stream_plan.append([self.ce_endpoint_stream_key])
+        return buffer_stream_plan
+
+    def create_buffer_stream_choices_plan(self, buffer_stream_entity):
+        buffer_stream_plan = self.create_buffer_stream_plan(buffer_stream_entity)
+        plan_cum_weight = None
+        return [(plan_cum_weight, buffer_stream_plan)]
+
+
+class WeightedRandomQoSSinglePolicySchedulerPlanner(BaseQoSSchedulerPlanner):
+    """
+    Bufferstream Choices Plan:
+        [<Corrected_dataflow_choice>]
+
+    Corrected_dataflow_choice:
+        [Dataflow cum. Weight, ([worker_stream_key], [worker_stream_key]...[ce_endpoint_stream_key])]
+
+    cumsum_dataflow Choices:
+        [best_weight_and_index_tuple, [<cumsum dataflow choice>]]
+
+    cumsum dataflow choice:
+        (Dataflow cum. weight, Dataflow Choice)
+
+    (Bufferstream) Dataflow Choice:
+        [<service worker weight tuple>]
+
+    service worker weight tuple:
+        (worker_weight, service, worker_stream_key)
+    """
+
+    def __init__(self, parent_service, scheduler_cmd_stream_key, ce_endpoint_stream_key):
+        super(SingleBestForQoSSinglePolicySchedulerPlanner, self).__init__(
+            parent_service,
+            scheduler_cmd_stream_key,
+            ce_endpoint_stream_key
         )
+        self.strategy_name = 'weighted_random'
 
-        return sorted_workers
+    def filter_available_service_worker_pool(self, worker_pool):
+        # dict(filter(
+        #     lambda x: self.calculate_worker_queue_space_percentage(x[1]) >= min_queue_space_percent, worker_pool.items()
+        # ))
+        return worker_pool
+
+    def get_worker_choice_weight(self, worker):
+        energy_consumption = worker['resources']['usage']['energy_consumption']
+        queue_space_percentage = self.calculate_worker_queue_space_percentage(worker)
+        # rating = space_perc * (1 / energy)
+        # the lower the energy consumption the higher the rating
+        # the higher the space, the higher the rating
+        if queue_space_percentage < 0:
+            queue_space_percentage = 0.001
+        rating = queue_space_percentage / (energy_consumption**2)
+        return rating
+
+    def get_dataflow_choice_min_weight(self, dataflow_choice):
+        if len(dataflow_choice) == 1:
+            dataflow_weight = dataflow_choice[0][-1]
+        else:
+            dataflow_weight = functools.reduce(
+                lambda w_tuple_a, w_tuple_b:
+                    min(w_tuple_a[-1], w_tuple_b[-1]),
+                dataflow_choice
+            )
+        return dataflow_weight
+
+    def calculate_cumsum_for_dataflow_choice(
+            self, dataflow_index, dataflow_choice, prev_cum_weight, best_weight_and_index):
+        # dataflow weight is the min weight of the whole dataflow workers
+        # (if one step is bad it will tend to reduce the whole dataflow to that level)
+        dataflow_weight = self.get_dataflow_choice_min_weight(dataflow_choice)
+        dataflow_cum_weight = prev_cum_weight + dataflow_weight
+        if best_weight_and_index is None:
+            best_weight_and_index = (dataflow_weight, dataflow_index)
+        else:
+            if dataflow_weight >= best_weight_and_index[0]:
+                best_weight_and_index = (dataflow_weight, dataflow_index)
+        prev_cum_weight = dataflow_cum_weight
+
+        dataflow_weighted_choice = [dataflow_cum_weight, dataflow_choice]
+
+        return best_weight_and_index, prev_cum_weight, dataflow_weighted_choice
+
+    def create_dataflow_choices_with_cum_weights_and_best_dataflow(self, per_service_workers_with_weights):
+        cartesian_product_dataflows_choices = itertools.product(*per_service_workers_with_weights.values())
+
+        dataflow_choices_with_cum_weights = []
+        best_weight_and_index = None
+        prev_cum_weight = 0
+        for i, dataflow_choice in enumerate(cartesian_product_dataflows_choices):
+            ret_tuple = self.calculate_cumsum_for_dataflow_choice(
+                i, dataflow_choice, prev_cum_weight, best_weight_and_index
+            )
+            best_weight_and_index, prev_cum_weight, dataflow_weighted_choice = ret_tuple
+            dataflow_choices_with_cum_weights.append(dataflow_weighted_choice)
+
+        return best_weight_and_index, dataflow_choices_with_cum_weights
+
+    # def update_workers_planned_resources(
+    #         self, dataflow_choice_weighted, dataflow_weight, total_cum_weight, min_queue_space_percent):
+    #     # Change of getting this dataflow
+    #     resource_usage_rating = (dataflow_weight / total_cum_weight)
+
+    #     # change of getting best dataflow times the expected usage before a new plan would be required
+    #     resource_usage = resource_usage_rating * min_queue_space_percent
+    #     cum_weight, dataflow = dataflow_choice_weighted
+    #     for worker in dataflow:
+    #         service_type, worker_key, _ = worker
+    #         actual_worker_reference = self.all_services_worker_pool[service_type][worker_key]
+    #         self.update_worker_planned_resource(actual_worker_reference, resource_usage)
 
     def create_buffer_stream_plan(self, buffer_stream_entity):
         required_services = self.get_buffer_stream_required_services(buffer_stream_entity)
@@ -80,12 +260,20 @@ class SingleBestForQoSSinglePolicySchedulerPlanner(BaseQoSSchedulerPlanner):
         buffer_stream_plan = []
         for service in required_services:
             worker_pool = self.all_services_worker_pool[service]
-            selected_worker_pool = worker_pool
+            selected_worker_pool = self.filter_available_service_worker_pool(worker_pool)
+            if len(selected_worker_pool) == 0:
+                selected_worker_pool = worker_pool
+
             qos_sorted_workers_keys = self.workers_key_sorted_by_qos(
                 selected_worker_pool, qos_policy_name, qos_policy_value)
 
             best_worker_key = qos_sorted_workers_keys[0]
             buffer_stream_plan.append([best_worker_key])
+
+            self.update_worker_planned_resource(
+                worker_pool[best_worker_key], min_queue_space_percent
+            )
+
         buffer_stream_plan.append([self.ce_endpoint_stream_key])
         return buffer_stream_plan
 
@@ -93,3 +281,5 @@ class SingleBestForQoSSinglePolicySchedulerPlanner(BaseQoSSchedulerPlanner):
         buffer_stream_plan = self.create_buffer_stream_plan(buffer_stream_entity)
         plan_cum_weight = None
         return [(plan_cum_weight, buffer_stream_plan)]
+
+
--- tests/test_query_qos_scheduling_planners.py
+++ tests/test_query_qos_scheduling_planners.py
@@ -44,6 +44,7 @@ class TestSingleBestForQoSSinglePolicySchedulerPlanner(TestCase):
                 'accuracy': '0.6',
                 # 'content_types': 'node:person',
                 'energy_consumption': '100.0',
+                'queue_size': 0,
                 'queue_limit': 100,
                 'queue_space': 100,
                 'queue_space_percent': 1.0,
@@ -66,6 +67,224 @@ class TestSingleBestForQoSSinglePolicySchedulerPlanner(TestCase):
                 'accuracy': '0.9',
                 # 'content_types': 'node_attribute:label',
                 'energy_consumption': '200.0',
+                'queue_size': 4,
+                'queue_limit': 100,
+                'queue_space': 100,
+                'queue_space_percent': 1.0,
+                'service_type': 'ObjectDetection',
+                'stream_key': 'object-detection-ssd-gpu-data',
+                'throughput': '30',
+                'type': 'http://gnosis-mep.org/service_worker'
+            },
+            'resources': {
+                'planned': {
+                    'queue_space': 100
+                },
+                'usage': {
+                    'energy_consumption': 108.0
+                }
+            }
+        }
+        self.all_services_worker_pool = {
+            'ObjectDetection': {
+                'object-detection-ssd-data': self.worker_a,
+                'object-detection-ssd-gpu-data': self.worker_b,
+            }
+        }
+
+    def test_workers_sorted_by_qos_accuracy_max(self):
+        worker_pool = self.all_services_worker_pool['ObjectDetection']
+        qos_policy_name = 'accuracy'
+        qos_policy_value = 'max'
+        ret = self.planner.workers_key_sorted_by_qos(
+            worker_pool, qos_policy_name=qos_policy_name, qos_policy_value=qos_policy_value)
+        self.assertListEqual(ret, ['object-detection-ssd-gpu-data', 'object-detection-ssd-data'])
+
+    def test_workers_sorted_by_qos_latency_min(self):
+        worker_pool = self.all_services_worker_pool['ObjectDetection']
+        qos_policy_name = 'latency'
+        qos_policy_value = 'min'
+        ret = self.planner.workers_key_sorted_by_qos(
+            worker_pool, qos_policy_name=qos_policy_name, qos_policy_value=qos_policy_value)
+        self.assertListEqual(ret, ['object-detection-ssd-gpu-data', 'object-detection-ssd-data'])
+
+    def test_workers_sorted_by_qos_energy_min(self):
+        worker_pool = self.all_services_worker_pool['ObjectDetection']
+        qos_policy_name = 'energy_consumption'
+        qos_policy_value = 'min'
+        ret = self.planner.workers_key_sorted_by_qos(
+            worker_pool, qos_policy_name=qos_policy_name, qos_policy_value=qos_policy_value)
+        self.assertListEqual(ret, ['object-detection-ssd-data', 'object-detection-ssd-gpu-data'])
+
+    def test_create_buffer_stream_plan_accuracy_max(self):
+        self.all_queries_dict['3940d2cad2926150093a9a786163ee14']['qos_policies'] = {
+            'accuracy': 'max'
+        }
+        bufferstream_entity = self.all_buffer_streams['b41eeb0408847b28474f362f5642635e']
+        self.planner.all_services_worker_pool = self.all_services_worker_pool
+        ret = self.planner.create_buffer_stream_plan(bufferstream_entity)
+        expected_bf_plan = [['object-detection-ssd-gpu-data'], ['wm-cmd']]
+        self.assertListEqual(ret, expected_bf_plan)
+
+    def test_create_buffer_stream_plan_latency_min(self):
+        self.all_queries_dict['3940d2cad2926150093a9a786163ee14']['qos_policies'] = {
+            'latency': 'min'
+        }
+        bufferstream_entity = self.all_buffer_streams['b41eeb0408847b28474f362f5642635e']
+        self.planner.all_services_worker_pool = self.all_services_worker_pool
+        ret = self.planner.create_buffer_stream_plan(bufferstream_entity)
+        expected_bf_plan = [['object-detection-ssd-gpu-data'], ['wm-cmd']]
+        self.assertListEqual(ret, expected_bf_plan)
+
+    def test_create_buffer_stream_plan_energy_min(self):
+        self.all_queries_dict['3940d2cad2926150093a9a786163ee14']['qos_policies'] = {
+            'energy_consumption': 'min'
+        }
+        bufferstream_entity = self.all_buffer_streams['b41eeb0408847b28474f362f5642635e']
+        self.planner.all_services_worker_pool = self.all_services_worker_pool
+        ret = self.planner.create_buffer_stream_plan(bufferstream_entity)
+        expected_bf_plan = [['object-detection-ssd-data'], ['wm-cmd']]
+        self.assertListEqual(ret, expected_bf_plan)
+
+    def test_initialize_service_workers_planned_capacity(self):
+        self.planner.adaptation_delta = 10
+        self.planner.all_services_worker_pool = self.all_services_worker_pool
+        worker_pool = self.planner.initialize_service_workers_planned_capacity(
+            self.planner.all_services_worker_pool['ObjectDetection'])
+        self.assertDictEqual(worker_pool, self.planner.all_services_worker_pool['ObjectDetection'])
+        self.assertEqual(
+            worker_pool['object-detection-ssd-data']['resources']['planned']['events_capacity'],
+            150)
+        self.assertEqual(
+            worker_pool['object-detection-ssd-gpu-data']['resources']['planned']['events_capacity'],
+            296)
+
+    def test_update_workers_planned_resources_when_capacity_is_present(self):
+        required_events = 101
+        required_services = ['ObjectDetection']
+        buffer_stream_plan = [['object-detection-ssd-data']]
+
+        self.planner.all_services_worker_pool = self.all_services_worker_pool
+        self.worker_a['resources']['planned']['events_capacity'] = 200
+
+        self.planner.update_workers_planned_resources(required_services, buffer_stream_plan, required_events)
+        self.assertEqual(
+            self.worker_a['resources']['planned']['events_capacity'],
+            99)
+
+    def test_update_workers_planned_resources_when_capacity_is_not_present(self):
+        required_events = 101
+        required_services = ['ObjectDetection']
+        buffer_stream_plan = [['object-detection-ssd-data']]
+
+        self.planner.all_services_worker_pool = self.all_services_worker_pool
+        self.worker_a['resources']['planned'].pop('events_capacity', None)
+
+        self.planner.update_workers_planned_resources(required_services, buffer_stream_plan, required_events)
+        self.assertEqual(
+            self.worker_a['resources']['planned']['events_capacity'],
+            -101)
+
+    def test_update_workers_planned_resources_when_more_than_one_required_service(self):
+        required_events = 10
+        required_services = ['ObjectDetection', 'ObjectDetection']
+        buffer_stream_plan = [['object-detection-ssd-data'], ['object-detection-ssd-gpu-data']]
+
+        self.planner.all_services_worker_pool = self.all_services_worker_pool
+        self.worker_a['resources']['planned']['events_capacity'] = 200
+        self.worker_b['resources']['planned']['events_capacity'] = 5
+
+        self.planner.update_workers_planned_resources(required_services, buffer_stream_plan, required_events)
+        self.assertEqual(
+            self.worker_a['resources']['planned']['events_capacity'],
+            190)
+
+        self.assertEqual(
+            self.worker_b['resources']['planned']['events_capacity'],
+            -5)
+
+    def test_filter_overloaded_service_worker_pool_or_all_if_empty_with_at_least_one(self):
+        self.planner.all_services_worker_pool = self.all_services_worker_pool
+        self.worker_a['resources']['planned']['events_capacity'] = 10
+        self.worker_b['resources']['planned']['events_capacity'] = -1
+
+        worker_pool = self.planner.filter_overloaded_service_worker_pool_or_all_if_empty(
+            self.planner.all_services_worker_pool['ObjectDetection'])
+        self.assertIn('object-detection-ssd-data', worker_pool.keys())
+        self.assertNotIn('object-detection-ssd-gpu-data', worker_pool.keys())
+
+    def test_filter_overloaded_service_worker_pool_or_all_if_empty_with_none(self):
+        self.planner.all_services_worker_pool = self.all_services_worker_pool
+        self.worker_a['resources']['planned']['events_capacity'] = 0
+        self.worker_b['resources']['planned']['events_capacity'] = -1
+
+        worker_pool = self.planner.filter_overloaded_service_worker_pool_or_all_if_empty(
+            self.planner.all_services_worker_pool['ObjectDetection'])
+        self.assertIn('object-detection-ssd-data', worker_pool.keys())
+        self.assertIn('object-detection-ssd-gpu-data', worker_pool.keys())
+
+class TestWeightedRandomQoSSinglePolicySchedulerPlanner(TestCase):
+
+    def setUp(self):
+        scheduler_cmd_stream_key = 'sc-cmd'
+        ce_endpoint_stream_key = 'wm-cmd'
+        parent_service = MagicMock()
+        self.planner = SingleBestForQoSSinglePolicySchedulerPlanner(
+            parent_service, scheduler_cmd_stream_key, ce_endpoint_stream_key)
+        self.all_queries_dict = {
+            '3940d2cad2926150093a9a786163ee14': {
+                'action': 'updateControlFlow',
+                'data_flow': ['ObjectDetection'],
+                'publisher_id': 'publisher1',
+                'qos_policies': {
+                    'accuracy': 'min'
+                },
+                'query_id': '3940d2cad2926150093a9a786163ee14',
+                'type': 'http://gnosis-mep.org/subscriber_query'
+            }
+        }
+        self.all_buffer_streams = {
+            'b41eeb0408847b28474f362f5642635e': {
+                'action': 'startPreprocessing',
+                'buffer_stream_key': 'b41eeb0408847b28474f362f5642635e',
+                'fps': '30',
+                'publisher_id': 'publisher1',
+                'queries': self.all_queries_dict,
+                'resolution': '640x480',
+                'source': 'rtmp://172.17.0.1/vod2/cars.mp4',
+                'type': 'http://gnosis-mep.org/buffer_stream'
+            }
+        }
+
+        self.worker_a = {
+            'monitoring': {
+                'accuracy': '0.6',
+                # 'content_types': 'node:person',
+                'energy_consumption': '100.0',
+                'queue_size': 4,
+                'queue_limit': 100,
+                'queue_space': 100,
+                'queue_space_percent': 1.0,
+                'service_type': 'ObjectDetection',
+                'stream_key': 'object-detection-ssd-data',
+                'throughput': '15',
+                'type': 'http://gnosis-mep.org/service_worker'
+            },
+            'resources': {
+                'planned': {
+                    'queue_space': 100
+                },
+                'usage': {
+                    'energy_consumption': 108.0
+                }
+            }
+        }
+        self.worker_b = {
+            'monitoring': {
+                'accuracy': '0.9',
+                # 'content_types': 'node_attribute:label',
+                'energy_consumption': '200.0',
+                'queue_size': 0,
                 'queue_limit': 100,
                 'queue_space': 100,
                 'queue_space_percent': 1.0,
